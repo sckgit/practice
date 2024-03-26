ee1fdef1407f3c3f01401ae10a3bfff110b03009
sckgit <1395195739@aliyun.com>
2024-03-26 16:33:25 +0800

    练习递归求数字和以及求n的k次方

---
+++ ConsoleApplication1/ConsoleApplication1/practise.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//函数实现求一个数每位数字之和
+double Digital_num(int n) {
+	if (n > 9) {
+		return Digital_num(n / 10) + n % 10;
+	}
+	else {
+		return n;
+	}
+}
+//函数实现n的k次方递归实现
+double Pow(double n,double k) {
+	if (k == 0) {
+		return 1.0;
+	}
+	else if (k >0) {
+		return n*Pow(n, k-1);
+	}
+	else {
+		return 1.0/(Pow(n,-k));
+	}
+}
+int main()
+{
+	double a = 5.0;
+	printf("%lf", Pow(a,-1.0));
+	return 0;
+}
+
+
