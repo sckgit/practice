e173cc07a01726cdf469ae2a49c0c4cd09c8dafa
sckgit <1395195739@aliyun.com>
2024-04-15 16:25:33 +0800

    第一次案例测试

---
+++ test1/test1.c
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#include<string.h>
+//求两个数最小公倍数
+//法一
+int min_zuixiaogongbeishu(int m, int n) {
+    int t = m > n ? m:n;
+    while (1) {
+        if ((t % m == 0) && (t % n == 0)) {
+            break;
+        }
+        t++;
+    }
+    return t;
+}
+//法二两数相乘除以最小公约数即是最小公倍数（辗转相除法）
+
+//题二-倒置字符串  1.整个字符串倒置  2.每个单词倒置
+void my_reverse(char* left, char* right) {
+    while (left < right) {
+        char tmp = *left;
+        *left = *right;
+        *right = tmp;
+        left++;
+        right--;
+    }
+}
+
+int main()
+{
+    char arr[1000] = { 0};
+    gets(arr);
+    int len = strlen(arr);
+    //1.整个字符串倒置
+    my_reverse(arr, arr + len - 1);
+    //2.倒置每个单词
+    char* start = arr;
+    while (*start) {
+        char* end = start;
+        while (*end != ' ' && *end != '\0') {    //单词结束可能为空格和斜杠0
+            end++;
+        }
+        my_reverse(start, end - 1);//倒置每个单词
+        if (end != ' ') {             //遇到空格 进行下一个单词start加一
+            start = end + 1;
+        }
+        else {
+            start = end;          //最后一个为斜杠0无需操作  
+        }
+    }
+}
+
--- zifuchuanmonishixian/zifuchuanmonishixian.c
+++ zifuchuanmonishixian/zifuchuanmonishixian.c
@@ -19,11 +19,11 @@ char* my_strcpy(char* dest,const char* src) {
 }
 
 //模拟实现strlwn函数实现字符串拷贝-strcpy函数实际返回目标空间起始地址
-int  my_strlen(const char* src) {
+size_t  my_strlen(const char* src) {
 	assert(src != NULL);
-	/*int count = 0;
+	/*size_t count = 0;
 	while (*(src++)!='\0') {
-		count++;
+		count++; 
 	}
 	return count;*/
 	if(*(src++) != '\0') {
@@ -34,6 +34,37 @@ int  my_strlen(const char* src) {
 	}
 }
 
+//计算参数n的二进制中有多少个1
+int Numberof1(int n) {
+	int count = 0;
+	int i = 0;
+	for(i=0;i<32;i++){
+		if (((n >> i) & 1) == 1) {
+			count++;
+		}
+	}
+}
+
+//计算参数n的二进制中有多少个1
+//int Numberof1(int n) {
+//	int count = 0;
+//	int i = 0;
+//	while(n){
+// n=n&(n-1);
+// count++;}
+// return count;
+//}
+
+//计算两个数中二进制位不同个数
+int erjinzhi_xiangyi(int m, int n) {
+	int count = 0;
+	for (int i = 0; i <= 32; i++) {
+		if (((m >> i) & 1) == ((n >> i) & 1)) {
+			count++;
+		}
+
+	}
+}
 int main()
 {
 	char arr1[20] = "xxxxxx";
